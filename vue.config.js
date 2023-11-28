ccfd258a73849fa07270411ab245fe4a379c6c15
SUNGHWON <donkihotex@gmail.com>
2023-11-28 19:57:48 +0900

    202311281957

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-<<<<<<< HEAD
 
-  // npm run build 타겟 디렉토리 (백엔드쪽!)
-  outputDir: "../allaboutu_springboot/src/main/resources/static",
-  
   // npm run server 개발 진행시 포트가 다르기 때문에 
   // 프록시 설정해줘야 함
   // target : 백엔드 port가 들어감
   // changeOrigin 
   // -> true로 해야 cros 문제 해결할 수 있음
-=======
+
   lintOnSave:false,
   outputDir: "../allaboutu_springboot/src/main/resources/static",
->>>>>>> c7427b9f2a618e36e4dff504378d640e535f7e1f
+
   devServer: {
     proxy: {
       '/': {
@@ -22,9 +18,6 @@ module.exports = defineConfig({
         changeOrigin: true,
       }
     }
-<<<<<<< HEAD
+
   }	
-=======
-  }   
->>>>>>> c7427b9f2a618e36e4dff504378d640e535f7e1f
 })
