059808670bf4f7cc9b6f1b8ac41415d637d4f32e
minji <minjijang21@gmail.com>
2023-11-29 17:42:23 +0900

    20231129minji push vue

--- src/views/admin/AdminMain.vue
+++ src/views/admin/AdminMain.vue
@@ -1,6 +1,16 @@
 <template>
     <PageHeader /><!-- 헤더 컴포넌트 -->
-    <AdminHome /><!-- 관리자 컴포넌트 -->
+    <div class="main-wrapper">
+        <div class="main-container">
+            <div class="contents-boxes">
+                <AdminHome /><!-- 관리자 게시글관리 컴포넌트 -->
+            </div>
+            <div class="contents-boxes">
+                <AdminUser /><!-- 관리자 회원관리 컴포넌트 -->
+            </div>
+        </div>
+    </div>
+
     <PageFooter /><!-- 푸터 컴포넌트 -->
 </template>
 
@@ -8,6 +18,7 @@
 import PageHeader from '@/views/common/PageHeader.vue'
 import AdminHome from '@/views/admin/AdminHome.vue'
 import PageFooter from '@/views/common/PageFooter.vue'
+import AdminUser from '@/views/admin/AdminUser.vue'
 
 export default ({
     name: 'AdminMain'
@@ -15,6 +26,13 @@ export default ({
         PageHeader
         ,AdminHome
         ,PageFooter
+        ,AdminUser
     }
 })
 </script>
+
+<style>
+.main-container {
+    display: flex;
+}
+</style>
