f2220ef2d3a08f8f1304513fe71f0ba9d10cfd76
SUNGHWON <donkihotex@gmail.com>
2023-12-05 14:45:22 +0900

    jsh231205 -1

--- src/views/style/StyleHome.vue
+++ src/views/style/StyleHome.vue
@@ -13,8 +13,13 @@
         <div class="pic-upload-box">
             <div class="pic-upload">
                 <label for="pic_up" ref="pic_label" :class="{ 'pic-label': imageUploaded}">업로드</label>
-                <input class="pic-img-box" id="pic_up" type="file" @change="handleFileUpload" accept="image/*">
-                <img class="my-img-box" :src="imageUrl" v-if="imageUrl" :style="{ display: imageUrl ? 'block' : 'none' }">
+                <input class="pic-img-box" type="file" id="pic_up"
+                    @change="handleFileUpload" accept="image/*"
+                >
+                <img class="my-img-box" 
+                    :src="imageUrl" v-if="imageUrl" 
+                    :style="{ display: imageUrl ? 'block' : 'none' }"
+                >
             </div>
             
             <div class="pic-btn-box">
@@ -52,16 +57,26 @@ import { ref } from 'vue';
 
 export default {
     emits: ['show-component'],
+    data() {
+        return {
+            tempUserNum : 1
+            ,orgFile: null
+        }
+    },
     setup() {
         const imageFile = ref(null);
         const imageUrl = ref(null);
         const imageUploaded = ref(false);
 
-        const handleFileUpload = (event) => {
-            const file = event.target.files[0];
+        const handleFileUpload = (e) => {
+            const file = e.target.files[0];
             imageFile.value = file;
             imageUrl.value = URL.createObjectURL(file);
             imageUploaded.value = true;
+            console.log('imageUrl.value : ' + imageUrl.value);
+            console.log('imageFile.value : ' + imageFile.value);
+            console.log('file : ' + file);
+            console.log('imageFile : ' + imageFile);
         };
 
         return {
@@ -74,10 +89,30 @@ export default {
     methods: {
         fnTypeClick(){
             
-
-
             this.$styleType.num = 1;
             this.$emit('show-component');
+
+            const formData = new FormData();
+            console.log('this.imageFile.value : ' + this.imageFile.value);
+            formData.append('file', this.imageFile);
+            formData.append('userNum', this.tempUserNum);
+
+            this.$axios.post('/style/upload', formData, {
+                headers:{
+                    'Content-Type': 'multipart/form-data'
+                }
+            })
+            .then(res => {
+                alert('사진 등록 성공');
+                console.log('res : ' + res);
+                console.log('res stringify : ' + JSON.stringify(res))
+            })
+            .catch(err => {
+                alert('게시글 등록 실패');
+                console.log(err);
+            })
+
+
         }
     }
 }
