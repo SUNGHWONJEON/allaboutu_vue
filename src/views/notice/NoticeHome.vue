c0499a9b52202dbbbcbff5e07bfc11b6ced69609
sungyoung <choisy7120@naver.com>
2023-12-07 18:08:31 +0900

    sungyoung

--- src/views/notice/NoticeHome.vue
+++ src/views/notice/NoticeHome.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="board-contents">
+  <div class="notice-contents">
     <div>
       <select id="noticeop" v-model="search_key">
         <option value="title">제목</option>
@@ -24,22 +24,26 @@
       </thead>
       <tr v-for="row in list" :key="row.noticeNum">
         <td>
-          {{ row.important ? '필독 ' : '' }}
-          {{ row.noticeNum }}
-        </td>
+            <!-- 필독 텍스트 -->
+            <span v-if="row.importance">필독 </span>
+            <!-- 공지번호 -->
+            {{ row.noticeNum }}
+       </td> 
         <td>
           <router-link :to="{ path: '/notice/detail/' + row.noticeNum }" :style="{ color: row.noticeTitle ? 'blue' : '', textDecoration: 'none' }" class="hover-effect">{{ row.noticeTitle }}</router-link>
         </td>
         <td>{{ row.userNum === 1 ? '관리자' : row.userNum }}</td>
         <td>{{ row.writeDate }}</td>
-        <td>{{ row.originalFileName ? '*' : '' }}</td>
+        <td>
+          <img v-if="row.renameFileName !== null" id="notice_img" src="@/assets/images/notice/attachment_87543.png" alt="Attachment Image">
+        </td>
       </tr>
     </table>
 
     <!-- 페이징 버튼 추가 -->
     <div>
       <button class="page-btn" @click="prevPage" :disabled="currentPage === 1">이전</button>
-      <button class="page-btn" @click="movePage(pageNumber)" v-for="pageNumber in pageNumbers" :key="pageNumber"> {{ pageNumber }}</button>
+      <button class="page-btn" @click="movePage(pageNumber)" v-for="pageNumber in pageNumbers" :class="{ 'current-page': isCurrentPage(pageNumber) }"  :key="pageNumber"> {{ pageNumber }}</button>
       <button class="page-btn" @click="nextPage" :disabled="currentPage === totalPages">다음</button>
     </div>
 
@@ -62,11 +66,17 @@ export default {
   },
   mounted() {
     this.fnPage();
+    console.log('Data:', this.list);
   },
   computed: {
     pageNumbers(){
       return Array.from({length: this.totalPages}, (_, index) => index +1);
-    }
+    },
+
+      isCurrentPage() {
+      return (pageNumber) => pageNumber === this.currentPage;
+    },
+    
   },
 
   methods: {
@@ -123,8 +133,8 @@ export default {
     }
 
     .page-btn{
-
-      margin: 5px;
+      font-size: 15px;
+      margin-left: 7px;
       color:#ad578c;
     }
     h3 {
@@ -135,12 +145,12 @@ export default {
     .w3-table-all {
           width: 950px;
           border-collapse: collapse;
-          margin: 70px;
-          margin-top: 30px;
+          margin-top: 45px;
+          
       }
     .register{
       margin-left: 500px;
-      background-color: #00ccff; /* 적절한 색상으로 변경 가능 */
+      background-color: #ad578c; /* 적절한 색상으로 변경 가능 */
       color: #fff; /* 텍스트 색상을 밝게 설정 */
       border: none;
       padding: 5px 10px;
@@ -166,8 +176,21 @@ export default {
 
     .searchtitle, .allList{
       color:#ad578c;
+      
     }
-  
+
+    .current-page {
+
+      background-color: #ad578c;
+      color: #fff;
+
+      width: 30px; 
+      height: 30px; 
+}
+  #notice_img{
+    width: 30px; 
+    height: 30px; 
+  }
 
    
 </style>
