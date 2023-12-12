b283b6b20700c3116bafa9d7e318d8894409e4be
Jimmy0216 <maximin910216@gmail.com>
2023-12-12 18:29:26 +0900

    1212_02

--- src/views/face/FaceHome.vue
+++ src/views/face/FaceHome.vue
@@ -22,20 +22,24 @@
                 <button class="pic-btn" v-on:click="fnTypeClick">적용한 사진 보기</button>
             </div>
 
-            <div class="pic-btn-box">
-                <button class="hair-btn" v-on:click="toggleHairOptions">Hair</button>
+            <div class="pic-btn-box color-box-face">
+
+                <div>
+                    <button class="hair-btn" v-on:click="toggleHairOptions">Hair</button>
+                    <button class="lip-btn" v-on:click="fnLipClick">Lip</button>
+                </div>
+                
                 <div class="hair-btn-container" v-if="hairOptionsVisible">
                     <button 
                         v-for="index in hairOptionCount"
                         :key="index"
                         class="hair-option-btn"
-                        v-show="showHairOptions"
                         @click="handleHairOptionClick(index)"
                     >
-                        Hair Option {{ index }}
+                        <img :src="require(`@/assets/images/face/hair/hair${index}.jpg`)">
                     </button>
                 </div>
-                <button class="lip-btn" v-on:click="fnLipClick">Lip</button>
+                
             </div>
         </div>
 
@@ -52,7 +56,7 @@ export default ({
       return{
         hairOptionsVisible: false,
         hairOptionCount: 7,
-        hairImagePath: 'E:\jm\AI\ai_workspace\face_makeup\color\hair'
+        hairImagePath: '../../src/assets/images/face/hair/'
       };  
     },
     setup() {
@@ -94,7 +98,7 @@ export default ({
             // 각 버튼 클릭 시 동작하는 로직 추가
         },
         getHairImageUrl(index){
-            return this.hairImagePath + 'hair-' + index + '.png';
+            return require(this.hairImagePath + 'hair' + index + '.jpg');
         },
         fnLipClick(){
 
@@ -103,12 +107,17 @@ export default ({
 })
 </script>
 
-<style>
+<style scoped>
 @import '@/assets/css/base_pic.css';
 
 .pic-btn-box {
   display: flex;
   gap: 10px; /* 버튼들 사이의 간격을 조정합니다. */
+}
+
+.color-box-face {
+    flex-direction: column;
+    align-items: center;
 }
 
 .hair-btn {
@@ -136,4 +145,20 @@ export default ({
   text-align: center;
   width: 100px;
 }
+
+.hair-btn-container {
+    width: 500px;
+}
+.hair-option-btn img{
+    width: 100px;
+    height: 100px;
+    border-radius: 100%;
+    margin: 5px;
+}
+
+.hair-option-btn:focus {
+  opacity: .6;
+
+}
+
 </style>
